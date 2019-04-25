9f862ae3568dc6ef55bb83ff176d2ef76107249c
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2019-04-26 00:28:56 +0800

    Templates & static (css)

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
-# Create your views here.
+# from django.http import HttpResponse 
+
+posts = [
+    {
+        'author': 'Qwek',
+        'title': 'Post 1',
+        'content': 'Helloworld!',
+        'date_posted': '25 April 2019'
+    },
+    {
+        'author': 'Zhi Hui',
+        'title': 'Post 2',
+        'content': '2nd post',
+        'date_posted': '25 April 2019'
+    }
+]
 
 def home(request): 
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context) # render returns http
     
 def about(request): 
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'}) # render returns http
+
+
+
+#  HTTPRESONSE EXAMPLE
+#  def about(request): 
+#     return HttpResponse('<h1>Blog About</h1>')
