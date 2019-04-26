c9d0b5182209183377829a2d70c158de2e27763e
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2019-04-26 23:53:41 +0800

    Model/Database

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #date_posted = models.DateTimeField(auto_now_add=True) #set to current date time on creation only
+    date_posted = models.DateTimeField(default=timezone.now) #consider timezone #pass in function but dont execute
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #if user is deleted, post is deleted as well
+
+    def __str__(self):
+        return self.title
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse 
 
-posts = [
-    {
-        'author': 'Qwek',
-        'title': 'Post 1',
-        'content': 'Helloworld!',
-        'date_posted': '25 April 2019'
-    },
-    {
-        'author': 'Zhi Hui',
-        'title': 'Post 2',
-        'content': '2nd post',
-        'date_posted': '25 April 2019'
-    }
-]
+# # Dummy posts 
+# posts = [
+#     {
+#         'author': 'Qwek',
+#         'title': 'Post 1',
+#         'content': 'Helloworld!',
+#         'date_posted': '25 April 2019'
+#     },
+#     {
+#         'author': 'Zhi Hui',
+#         'title': 'Post 2',
+#         'content': '2nd post',
+#         'date_posted': '25 April 2019'
+#     }
+# ]
 
 def home(request): 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context) # render returns http
     
 def about(request): 
     return render(request, 'blog/about.html', {'title' : 'About'}) # render returns http
-
-
 
 #  HTTPRESONSE EXAMPLE
 #  def about(request): 
