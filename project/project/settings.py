1bdb18ae6be235631750401104d91c4b243d5d3d
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2019-05-15 04:08:54 +0800

    authviews (w/ environ var) PasswordResetView PasswordResetDoneView PasswordResetConfirmView PasswordResetDoneView

--- project/project/settings.py
+++ project/project/settings.py
@@ -123,3 +123,11 @@ LOGIN_URL = 'login'
 # Directory for pictures (file stored on /media not in DB)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'MoiKeyboard@gmail.com'
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
